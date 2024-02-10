c70479776e0b5f6b5b41a251369f24bbf48ffe59
Jin Jung <145183260+aspective04@users.noreply.github.com>
2024-02-10 18:34:35 +0900

    TokenManager 추가 및 Token 움직임 변경 (#43)
    * Updated DOTween & Added TokenManager & Reworked Token movement
    * Added finishedPosition into GetNextPosition() & Added ClearPreviousPositions() to Token & Fixed ResetToken()
    * Added function descriptions

---
+++ Assets/Scripts/Managers/TokenManager.cs
@@ -0,0 +1,219 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public enum boardPointIndex
+{
+    LowerRight,
+    Right1,
+    Right2,
+    Right3,
+    Right4,
+    UpperRight,
+    Upper1,
+    Upper2,
+    Upper3,
+    Upper4,
+    UpperLeft,
+    Left1,
+    Left2,
+    Left3,
+    Left4,
+    LowerLeft,
+    Lower1,
+    Lower2,
+    Lower3,
+    Lower4,
+    RightDiag1,
+    RightDiag2,
+    Center,
+    RightDiag3,
+    RightDiag4,
+    LeftDiag1,
+    LeftDiag2,
+    LeftDiag3,
+    LeftDiag4
+}
+
+public class TokenManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> boardPoints;
+
+    [SerializeField] private GameObject tokenPrefab1, tokenPrefab2;
+    [SerializeField] private List<Vector2> initialPositions1, initialPositions2;
+
+    private List<Token> tokens1, tokens2;
+
+    private void Start()
+    {
+        tokens1 = new List<Token>();
+        tokens2 = new List<Token>();
+
+        for (int i = 0; i < initialPositions1.Count; i++)
+        {
+            var newToken = Instantiate<GameObject>(tokenPrefab1, initialPositions1[i], Quaternion.identity);
+            tokens1.Add(newToken.GetComponent<Token>());
+        }
+        for (int i = 0; i < initialPositions1.Count; i++)
+        {
+            var newToken = Instantiate<GameObject>(tokenPrefab2, initialPositions2[i], Quaternion.identity);
+            tokens2.Add(newToken.GetComponent<Token>());
+        }
+
+        ResetToken(tokens1[0]);
+    }
+
+    private void Update()
+    {
+        if (GetBoardPointIndex(tokens1[0]) == -1) return;
+        DebugHandleInput();
+    }
+
+    /// <summary>
+    /// Clears previousPositions of token and moves it to lowerRightPoint
+    /// </summary>
+    /// <param name="token"></param>
+    private void ResetToken(Token token)
+    {
+        token.ClearPreviousPositions();
+        StartCoroutine(token.MoveTo(boardPoints[(int)boardPointIndex.LowerRight]));
+    }
+
+    /// <summary>
+    /// Gets index of the boardPoint that token is on; -1 if none
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    private int GetBoardPointIndex(Token token)
+    {
+        for (int index = 0; index < boardPoints.Count; index++)
+        {
+            if (token.IsTokenAt(boardPoints[index])) return index;
+        }
+        return -1;
+    }
+
+    /// <summary>
+    /// Gets the next position that token has to go to from current position
+    /// </summary>
+    /// <param name="token"></param>
+    /// <param name="isFirstMove"></param>
+    /// <returns>nextPosition; if finished, finishedPosition</returns>
+    private Vector2 GetNextPosition(Token token, bool isFirstMove)
+    {
+        int index = GetBoardPointIndex(token);
+        if (index == -1) return Vector2.zero;
+        if (index == (int)boardPointIndex.LowerRight && 
+            token.CountPreviousPositions() != 0 && (
+            token.PeekPreviousPositions() == (Vector2)boardPoints[(int)boardPointIndex.Lower4].transform.position ||
+            token.PeekPreviousPositions() == (Vector2)boardPoints[(int)boardPointIndex.LeftDiag4].transform.position))
+        {
+            token.IsFinished = true;
+            Vector2 finishedPosition = boardPoints[(int)boardPointIndex.LowerRight].transform.position;
+            finishedPosition.y -= 15f;
+            return finishedPosition;
+        }
+
+        if (isFirstMove)
+        {
+            if (index == (int)boardPointIndex.UpperRight)
+            {
+                return boardPoints[(int)boardPointIndex.RightDiag1].transform.position;
+            }
+            else if (index == (int)boardPointIndex.UpperLeft)
+            {
+                return boardPoints[(int)boardPointIndex.LeftDiag1].transform.position;
+            }
+            else if (index == (int)boardPointIndex.Center)
+            {
+                return boardPoints[(int)boardPointIndex.LeftDiag3].transform.position;
+            }
+        }
+        if (index == (int)boardPointIndex.Center &&
+            token.PeekPreviousPositions() == (Vector2)boardPoints[(int)boardPointIndex.LeftDiag2].transform.position)
+        {
+            return boardPoints[(int)boardPointIndex.LeftDiag3].transform.position;
+        }
+        if (index == (int)boardPointIndex.LeftDiag2)
+        {
+            return boardPoints[(int)boardPointIndex.Center].transform.position;
+        }
+        if (index == (int)boardPointIndex.LeftDiag4)
+        {
+            return boardPoints[(int)boardPointIndex.LowerRight].transform.position;
+        }
+        if (index == (int)boardPointIndex.RightDiag4)
+        {
+            return boardPoints[(int)boardPointIndex.LowerLeft].transform.position;
+        }
+        if (index == (int)boardPointIndex.Lower4)
+        {
+            return boardPoints[(int)boardPointIndex.LowerRight].transform.position;
+        }
+        return boardPoints[index + 1].transform.position;
+    }
+
+    /// <summary>
+    /// Actually moves token according to GetNextPosition(); Use with StartCoroutine()
+    /// </summary>
+    /// <param name="token"></param>
+    /// <param name="distance"></param>
+    /// <returns></returns>
+    private IEnumerator MoveToken(Token token, int distance)
+    {
+        if (distance < 0)
+        {
+            if (token.CountPreviousPositions() == 0) yield return null;
+            yield return token.MoveTo(token.PopPreviousPositions());
+        }
+
+        for (int i = 0; i < distance; i++)
+        {
+            Vector2 nextPosition = GetNextPosition(token, (i == 0) ? true : false);
+            token.RecordPosition();
+            yield return token.MoveTo(nextPosition);
+        }
+    }
+
+    /// <summary>
+    /// Starts a coroutine that moves token by distance
+    /// </summary>
+    /// <param name="token"></param>
+    /// <param name="distance"></param>
+    private void StartMove(Token token, int distance)
+    {
+        if (token.IsFinished) return;
+        if (distance < 0 && token.CountPreviousPositions() == 0) return;
+        StartCoroutine(MoveToken(token, distance));
+    }
+
+    /// <summary>
+    /// A debug function that handles keyboard input
+    /// </summary>
+    private void DebugHandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            StartMove(tokens1[0], 1);
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            StartMove(tokens1[0], 2);
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            StartMove(tokens1[0], 3);
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            StartMove(tokens1[0], -1);
+            return;
+        }
+    }
+}
--- Assets/Scripts/Token/Token.cs
+++ Assets/Scripts/Token/Token.cs
@@ -2,120 +2,26 @@ using UnityEngine;
 using DG.Tweening;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using System.Collections;
 
 public class Token : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> rightPoints;
-    [SerializeField] private List<GameObject> upperPoints;
-    [SerializeField] private List<GameObject> leftPoints;
-    [SerializeField] private List<GameObject> lowerPoints;
-    [SerializeField] private List<GameObject> rightDiagPoints;
-    [SerializeField] private List<GameObject> leftDiagPoints;
-    [SerializeField] private GameObject centralPoint;
-    [SerializeField] private GameObject lowerRightPoint; // The starting point
-    [SerializeField] private GameObject upperRightPoint;
-    [SerializeField] private GameObject upperLeftPoint;
-    [SerializeField] private GameObject lowerLeftPoint;
-
-    private int totalDist;    // Distance that token has to move on this turn; Negative values represent backward movement
-    private int movedDist;    /// Distance that token has moved on this turn
-
+    private bool isFinished;    /// Whether the token has completed a lap
     private Stack<Vector2> previousPositions;     /// Stores the token's previous positions for backward movement
 
-    private bool finished;    /// Whether the token has completed a lap
+    public bool IsFinished { get; set; }
 
-    private void Start()
+    private void Awake()
     {
-        totalDist = movedDist = 0;
-        finished = false;
+        isFinished = false;
         previousPositions = new Stack<Vector2>();
-        MoveTo(lowerRightPoint);
     }
 
-    /// <summary>
-    /// If the token is finished or it is moving, return. If the token is done moving, accept inputs and return.
-    /// Else, find where the token is and move it accordingly.
-    /// </summary>
     private void Update()
     {
-        if (finished || !IsOnBoardPoint()) return;
-        if (IsDoneMoving())
-        {
-            DebugHandleKeyPress();
-            return;
-        }
-        if (totalDist < 0)
-        {
-            MoveBackwards();
-            return;
-        }
-        int index = CheckRightPoints();
-        if (index != -1)
-        {
-            MoveFromRightPoints(index);
-            return;
-        }
-        index = CheckUpperPoints();
-        if (index != -1)
-        {
-            MoveFromUpperPoints(index);
-            return;
-        }
-        index = CheckLeftPoints();
-        if (index != -1)
-        {
-            MoveFromLeftPoints(index);
-            return;
-        }
-        index = CheckLowerPoints();
-        if (index != -1)
-        {
-            MoveFromLowerPoints(index);
-            return;
-        }
-        index = CheckRightDiagPoints();
-        if (index != -1)
-        {
-            MoveFromRightDiagPoints(index);
-            return;
-        }
-        index = CheckLeftDiagPoints();
-        if (index != -1)
-        {
-            MoveFromLeftDiagPoints(index);
-            return;
-        }
-        index = CheckCentralPoint();
-        if (index != -1)
-        {
-            MoveFromCentralPoint();
-            return;
-        }
-        index = CheckLowerRightPoint();
-        if (index != -1)
-        {
-            MoveFromLowerRightPoint();
-            return;
-        }
-        index = CheckUpperRightPoint();
-        if (index != -1)
-        {
-            MoveFromUpperRightPoint();
-            return;
-        }
-        index = CheckUpperLeftPoint();
-        if (index != -1)
-        {
-            MoveFromUpperLeftPoint();
-            return;
-        }
-        index = CheckLowerLeftPoint();
-        if (index != -1)
-        {
-            MoveFromLowerLeftPoint();
-            return;
-        }
+
     }
+
     /// <summary>
     /// Moves token to newPosition instantly
     /// </summary>
@@ -124,6 +30,7 @@ public class Token : MonoBehaviour
     {
         transform.position = newPosition;
     }
+
     /// <summary>
     /// Moves token to boardPoint instantly
     /// </summary>
@@ -132,24 +39,27 @@ public class Token : MonoBehaviour
     {
         InstantMoveTo(boardPoint.transform.position);
     }
+
     /// <summary>
-    /// Moves token to newPosition smoothly
+    /// Moves token to newPosition smoothly; Use with StartCoroutine()
     /// </summary>
     /// <param name="newPosition"></param>
-    public void MoveTo(Vector2 newPosition)
+    public IEnumerator MoveTo(Vector2 newPosition)
     {
-        transform.DOMove(newPosition, 0.5f);
+        yield return transform.DOMove(newPosition, 0.5f).WaitForCompletion();
     }
+
     /// <summary>
-    /// Moves token to boardPoint smoothly
+    /// Moves token to boardPoint smoothly; Use with StartCoroutine()
     /// </summary>
     /// <param name="boardPoint"></param>
-    public void MoveTo(GameObject boardPoint)
+    public IEnumerator MoveTo(GameObject boardPoint)
     {
-        MoveTo(boardPoint.transform.position);
+        yield return MoveTo(boardPoint.transform.position);
     }
+
     /// <summary>
-    /// Returns as bool whether the Token is at checkPosition
+    /// Returns whether the Token is at checkPosition
     /// </summary>
     /// <param name="checkPosition"></param>
     /// <returns></returns>
@@ -157,8 +67,9 @@ public class Token : MonoBehaviour
     {
         return transform.position.x == checkPosition.x && transform.position.y == checkPosition.y;
     }
+
     /// <summary>
-    /// Returns as bool whether the Token is at the same position as boardPoint
+    /// Returns whether the Token is at the same position as boardPoint
     /// </summary>
     /// <param name="boardPoint"></param>
     /// <returns></returns>
@@ -166,318 +77,40 @@ public class Token : MonoBehaviour
     {
         return IsTokenAt(boardPoint.transform.position);
     }
-    /// <summary>
-    /// Sets token's totalDist to given value and resets movedDist to 0
-    /// </summary>
-    /// <param name="totalDist"></param>
-    public void StartMove(int totalDist)
+
+    public void ClearPreviousPositions()
     {
-        this.totalDist = totalDist;
-        movedDist = 0;
+        previousPositions.Clear();
     }
+
     /// <summary>
-    /// Returns as bool whether the token's movedDist is equal to totalDist
+    /// Pushes Token's current position into previousPositions
     /// </summary>
-    /// <returns></returns>
-    public bool IsDoneMoving() { return movedDist == totalDist; }
-    /// <summary>
-    /// Increases movedDist by 1 and pushes current position into previousPositions
-    /// </summary>
-    private void HandleEndOfMove()
+    public void RecordPosition()
     {
-        movedDist++;
         previousPositions.Push(transform.position);
     }
-    /// <summary>
-    /// Checks if the token is on rightPoints
-    /// </summary>
-    /// <returns>If on rightPoints, the index. If not, -1.</returns>
-    private int CheckRightPoints()
+
+    public int CountPreviousPositions()
     {
-        for (int i = 0; i < 4; i++)
-        {
-            if (IsTokenAt(rightPoints[i])) return i;
-        }
-        return -1;
-    }
-    /// <summary>
-    /// Moves the token from rightPoints[i]
-    /// </summary>
-    /// <param name="i"></param>
-    private void MoveFromRightPoints(int i)
-    {
-        if (i == 3) MoveTo(upperRightPoint);
-        else MoveTo(rightPoints[i + 1]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Checks if the token is on upperPoints
-    /// </summary>
-    /// <returns>If on upperPoints, the index. If not, -1.</returns>
-    private int CheckUpperPoints()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            if (IsTokenAt(upperPoints[i])) return i;
-        }
-        return -1;
-    }
-    /// <summary>
-    /// Moves the token from upperPoints[i]
-    /// </summary>
-    /// <param name="i"></param>
-    private void MoveFromUpperPoints(int i)
-    {
-        if (i == 3) MoveTo(upperLeftPoint);
-        else MoveTo(upperPoints[i + 1]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Checks if the token is on leftPoints
-    /// </summary>
-    /// <returns>If on leftPoints, the index. If not, -1.</returns>
-    private int CheckLeftPoints()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            if (IsTokenAt(leftPoints[i])) return i;
-        }
-        return -1;
-    }
-    /// <summary>
-    /// Moves the token from leftPoints[i]
-    /// </summary>
-    /// <param name="i"></param>
-    private void MoveFromLeftPoints(int i)
-    {
-        if (i == 3) MoveTo(lowerLeftPoint);
-        else MoveTo(leftPoints[i + 1]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Checks if the token is on lowerPoints
-    /// </summary>
-    /// <returns>If on lowerPoints, the index. If not, -1.</returns>
-    private int CheckLowerPoints()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            if (IsTokenAt(lowerPoints[i])) return i;
-        }
-        return -1;
-    }
-    /// <summary>
-    /// Moves the token from upperPoints[i]
-    /// </summary>
-    /// <param name="i"></param>
-    private void MoveFromLowerPoints(int i)
-    {
-        if (i == 3) MoveTo(lowerRightPoint);
-        else MoveTo(lowerPoints[i + 1]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Checks if the token is on rightDiagPoints
-    /// </summary>
-    /// <returns>If on rightDiagPoints, the index. If not, -1.</returns>
-    private int CheckRightDiagPoints()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            if (IsTokenAt(rightDiagPoints[i])) return i;
-        }
-        return -1;
-    }
-    /// <summary>
-    /// Moves the token from rightDiagPoints[i]
-    /// </summary>
-    /// <param name="i"></param>
-    private void MoveFromRightDiagPoints(int i)
-    {
-        if (i == 1) MoveTo(centralPoint);
-        else if (i == 3) MoveTo(lowerLeftPoint);
-        else MoveTo(rightDiagPoints[i + 1]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Checks if the token is on leftDiagPoints
-    /// </summary>
-    /// <returns>If on leftDiagPoints, the index. If not, -1.</returns>
-    private int CheckLeftDiagPoints()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            if (IsTokenAt(leftDiagPoints[i])) return i;
-        }
-        return -1;
+        return previousPositions.Count;
     }
 
     /// <summary>
-    /// Moves the token from leftDiagPoints[i]
-    /// </summary>
-    /// <param name="i"></param>
-    private void MoveFromLeftDiagPoints(int i)
-    {
-        if (i == 1) MoveTo(centralPoint);
-        else if (i == 3) MoveTo(lowerRightPoint);
-        else MoveTo(leftDiagPoints[i + 1]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Checks if the token is on centralPoint
-    /// </summary>
-    /// <returns>If on centralPoint, 0. If not, -1.</returns>
-    private int CheckCentralPoint()
-    {
-        if (IsTokenAt(centralPoint)) return 0;
-        return -1;
-    }
-
-    /// <summary>
-    /// Moves the token from centralPoint
-    /// </summary>
-    private void MoveFromCentralPoint()
-    {
-        if (movedDist == 0 || previousPositions.Peek() == (Vector2)leftDiagPoints[1].transform.position)
-            MoveTo(leftDiagPoints[2]);
-        else MoveTo(rightDiagPoints[2]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Checks if the token is on lowerRightPoint
-    /// </summary>
-    /// <returns>If on lowerRightPoint, 0. If not, -1.</returns>
-    private int CheckLowerRightPoint()
-    {
-        if (IsTokenAt(lowerRightPoint)) return 0;
-        return -1;
-    }
-    /// <summary>
-    /// Moves the token from lowerRightPoint and checks if the token has completed a lap
-    /// </summary>
-    private void MoveFromLowerRightPoint()
-    {
-        if (previousPositions.Count > 0 && (
-            previousPositions.Peek() == (Vector2)lowerPoints[3].transform.position ||
-            previousPositions.Peek() == (Vector2)leftDiagPoints[3].transform.position)) finished = true;
-        else MoveTo(rightPoints[0]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Checks if the token is on upperRightPoint
-    /// </summary>
-    /// <returns>If on upperRightPoint, 0. If not, -1.</returns>
-    private int CheckUpperRightPoint()
-    {
-        if (IsTokenAt(upperRightPoint)) return 0;
-        return -1;
-    }
-    /// <summary>
-    /// Moves the token from upperRightPoint
-    /// </summary>
-    private void MoveFromUpperRightPoint()
-    {
-        if (movedDist == 0) MoveTo(rightDiagPoints[0]);
-        else MoveTo(upperPoints[0]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Checks if the token is on upperLeftPoint
-    /// </summary>
-    /// <returns>If on upperLeftPoint, 0. If not, -1.</returns>
-    private int CheckUpperLeftPoint()
-    {
-        if (IsTokenAt(upperLeftPoint)) return 0;
-        return -1;
-    }
-    /// <summary>
-    /// Moves the token from upperLeftPoint
-    /// </summary>
-    private void MoveFromUpperLeftPoint()
-    {
-        if (movedDist == 0) MoveTo(leftDiagPoints[0]);
-        else MoveTo(leftPoints[0]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Checks if the token is on lowerLeftPoint
-    /// </summary>
-    /// <returns>If on lowerLeftPoint, 0. If not, -1.</returns>
-    private int CheckLowerLeftPoint()
-    {
-        if (IsTokenAt(lowerLeftPoint)) return 0;
-        return -1;
-    }
-    /// <summary>
-    /// Moves the token from lowerLeftPoint
-    /// </summary>
-    private void MoveFromLowerLeftPoint()
-    {
-        MoveTo(lowerPoints[0]);
-        HandleEndOfMove();
-    }
-    /// <summary>
-    /// Returns whether the token is on any boardPoint
+    /// Pops and returns top of previousPositions
     /// </summary>
     /// <returns></returns>
-    private bool IsOnBoardPoint()
+    public Vector2 PopPreviousPositions()
     {
-        return
-            CheckRightPoints() != -1 ||
-            CheckUpperPoints() != -1 ||
-            CheckLeftPoints() != -1 ||
-            CheckLowerPoints() != -1 ||
-            CheckRightDiagPoints() != -1 ||
-            CheckLeftDiagPoints() != -1 ||
-            CheckCentralPoint() != -1 ||
-            CheckLowerRightPoint() != -1 ||
-            CheckUpperRightPoint() != -1 ||
-            CheckUpperLeftPoint() != -1 ||
-            CheckLowerLeftPoint() != -1;
+        return previousPositions.Pop();
     }
+
     /// <summary>
-    /// Moves the token backwards
+    /// Returns top of previousPositions
     /// </summary>
-    private void MoveBackwards()
+    /// <returns></returns>
+    public Vector2 PeekPreviousPositions()
     {
-        if (previousPositions.Count > 0)
-            MoveTo(previousPositions.Pop());
-        movedDist--;
-    }
-    /// <summary>
-    /// Detects number key presses and starts moves accordingly
-    /// </summary>
-    private void DebugHandleKeyPress()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            StartMove(1);
-            return;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            StartMove(2);
-            return;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            StartMove(3);
-            return;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            StartMove(4);
-            return;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            StartMove(5);
-            return;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            StartMove(-1);
-            return;
-        }
+        return previousPositions.Peek();
     }
 }
