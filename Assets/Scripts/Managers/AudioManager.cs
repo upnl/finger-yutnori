43b5d700461c18aa614c6768d7f66e615bdc266b
Minhyeok OH <110721617+cute-gorilla@users.noreply.github.com>
2024-02-21 00:35:07 +0900

    Add SoundManager & Add text animation (#69)

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource bgmPlayer;
+    [SerializeField] private AudioSource sfxPlayer;
+    
+    [SerializeField] private AudioClip bgmClip;
+    [SerializeField] private AudioClip[] sfxClips;
+
+    private void Awake()
+    {
+        bgmPlayer.clip = bgmClip;
+    }
+
+    public void PlayBGM()
+    {
+        bgmPlayer.Play();
+    }
+
+    public void StopBGM()
+    {
+        bgmPlayer.Stop();
+    }
+
+    public void PlaySfx(int index)
+    {
+        sfxPlayer.PlayOneShot(sfxClips[index]);
+    }
+
+    void Start()
+    {
+        
+    }
+    
+}
