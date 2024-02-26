e848e188b9a9bd3e8cad63f170cdfdeef20b8124
minhyuk <minhyuk5@snu.ac.kr>
2024-02-26 19:16:18 +0900

    add comment

--- Assets/Scripts/Managers/LoginManager.cs
+++ Assets/Scripts/Managers/LoginManager.cs
@@ -46,14 +46,14 @@ public class LoginManager : MonoBehaviour
     
     void Update()
     {
-	    if (sameNameText.gameObject.activeSelf)
+	    if (sameNameText.gameObject.activeSelf) //플레이어이름 중복텍스트의 애니메이션 효과
 	    {
 		    sameNameText.transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
 		    alphaSameColor.a = Mathf.Lerp(alphaSameColor.a, 0, Time.deltaTime * alphaSpeed);
 		    sameNameText.color = alphaSameColor;
 	    }
 	    
-	    if (emptyNameText.gameObject.activeSelf)
+	    if (emptyNameText.gameObject.activeSelf) //플레이어이름 empty텍스트의 애니메이션 효과
 	    {
 		    emptyNameText.transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
 		    alphaEmptyColor.a = Mathf.Lerp(alphaEmptyColor.a, 0, Time.deltaTime * alphaSpeed);
@@ -61,7 +61,7 @@ public class LoginManager : MonoBehaviour
 	    }
     }
 
-	public void ClickContinueButton()
+	public void ClickContinueButton() //확인버튼을 눌렀을 때
 	{
 		List<string> playerHandList = new() { "LeftHand", "RightHand" };
 		if (player1ToggleGroup.AnyTogglesOn() == false) player1Hand = playerHandList[Random.Range(0, 2)];
@@ -69,19 +69,19 @@ public class LoginManager : MonoBehaviour
 
         if (player2ToggleGroup.AnyTogglesOn() == false) player2Hand = playerHandList[Random.Range(0, 2)];
         else player2Hand = player2ToggleGroup.ActiveToggles().FirstOrDefault().name;
-		//플레이어가 선택한 손을 받아온다.
-
-
+        //플레이어가 선택한 손을 저장한다. 선택하지 않을 시 랜덤.
+        
 		player1Name = player1InputField.transform.Find("InputPlayerName").GetComponent<TMP_InputField>().text;
 		player2Name = player2InputField.transform.Find("InputPlayerName").GetComponent<TMP_InputField>().text;
 		//inputfield에 입력받은 플레이어의 이름을 받아온다.
-		if (player1Name.Equals("") || player2Name.Equals(""))
+		
+		if (player1Name.Equals("") || player2Name.Equals("")) 
 		{
 			emptyNameText.gameObject.SetActive(true);
 			emptyNameText.transform.localPosition = new Vector3(0,500,0);
 			alphaEmptyColor.a = 1f;
 			return;
-		}
+		} //한 플레이어라도 이름을 입력하지 않을 때 에러텍스트를 띄운다.
 			
 		if (player1Name.Equals(player2Name))
 		{
@@ -89,7 +89,7 @@ public class LoginManager : MonoBehaviour
 			sameNameText.transform.localPosition = new Vector3(0, 500, 0);
 			alphaSameColor.a = 1f;
 			return;
-		}
+		} //두 플레이어의 이름이 같을 때 에러텍스트를 띄운다.
 		
 		PlayerPrefs.SetString("player1Name", player1Name);
 		PlayerPrefs.SetString("player2Name", player2Name);
